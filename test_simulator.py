819c80f647888979b16ed32947b022bde174ba88
BenjaminASwenson <BSwenson@spartaglobal.com>
2022-07-18 17:00:25 +0100

    first commit

---
+++ test_simulator.py
@@ -0,0 +1,82 @@
+# get_num_of_open_centers()
+# get_num_of_full_centers()
+# get_num_working_trainees()
+# get_num_waiting_list()
+
+
+# generate_new_trainees()
+# distribute_trainees()
+
+# open_centers - list
+# num_waiting_list - integer
+
+
+from tkinter import *
+import unittest
+from simulator import Simulator
+
+class Simulator_tests(unittest.TestCase):
+
+
+    # test whether or not the simulator is an instance of the class
+    def test_instance(self):
+        simulator = Simulator()
+
+        self.assertIsInstance(simulator, Simulator, "Failed to create an object of the class Simulator.")
+
+    # # test the data types of the input for the simulator
+    # def test_input_type(self):
+    #     self.assertEqual(self.simulator_string.input, False, "The class accepts non-integers as inputs.")
+    #     self.assertEqual(self.simulator_float.input, False, "The class accepts floats as inputs.")
+    #     self.assertEqual(self.simulator_negative.input, False, "The class accepts negative values as inputs.")
+
+    # test the number of open centres
+    def test_number_of_open_centers(self):
+        simulator = Simulator()
+        self.assertEqual(simulator.calculate_open_centres(), 6,
+                         "The number of open centres is not correct.")
+
+    # test the number of full centres
+    def test_number_of_full_centers(self):
+        simulator = Simulator()
+        self.assertEqual(simulator.calculate_full_centres(), 1,
+                         "The number of full centres is not correct.")
+
+    # test the number of working trainees
+    def test_number_working_trainees(self):
+        simulator = Simulator()
+        self.assertEqual(simulator.calculate_num_of_trainees(), 3,
+                         "The number of trainees currently training is wrong.")
+
+    # test how many trainees are waiting on the list
+    def test_get_waiting_trainees(self):
+        simulator = Simulator()
+        self.assertEqual(simulator.calculate_num_of_waiting_list(), 2,
+                         "We're getting the wrong number or waiting list trainees")
+
+    def test_calculate_closed_centres(self):
+        simulator = Simulator()
+        self.assertEqual(simulator.calculate_closed_centres(), 5, "Wrong number of closed centres")
+
+    def test_add_to_history(self):
+        simulator = Simulator()
+        simulator.add_to_history()
+        self.assertEqual(len(simulator.history), 1, "Wrong length")
+
+    # # testing the output where one month was entered in simulator
+    # def test_simulator1_outputs(self):
+    #     self.assertEqual(self.simulator1.open_centers[0], 13, "Failed to assign trainees to the open center in a Simulator object with input 1.")
+    #     self.assertEqual(self.simulator1.num_waiting_list, 76, "Failed to put extra trainees into the waiting list in a Simulator object with input 1.")
+
+
+    #TKinter BELOW
+
+    # tests whether the simulator title is a string
+    # def test_simulator_title_is_string(self):
+    #     title=self.simulation_interface.winfo_toplevel().title()
+    #     self.assertEqual(title, "Simulator")
+
+
+
+
+
